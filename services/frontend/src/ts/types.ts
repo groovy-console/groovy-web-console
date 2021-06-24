a7575b81934da7b1bced3effefdbc3e34f0d2121
Leonard Brünings <lord_damokles@gmx.net>
2021-06-24 20:08:02 +0200

    Add editorconfig, Add lint to workflow

--- services/frontend/src/ts/types.ts
+++ services/frontend/src/ts/types.ts
@@ -1,16 +1,16 @@
 export interface ExecutionResult {
-    out: string
-    err: string
-    result: string
+  out: string
+  err: string
+  result: string
 }
 
 export interface GistFile {
-    truncated: boolean
-    language: string
-    content: string
+  truncated: boolean
+  language: string
+  content: string
 }
 
 // Minimal interface for the fields that interest us from the response
 export interface GistResponse {
-    files: Map<string, GistFile>
+  files: Map<string, GistFile>
 }
