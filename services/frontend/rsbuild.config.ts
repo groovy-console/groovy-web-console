eb2da734db4f4e98becb0153192f7aab47068a39
Leonard Brünings <lord_damokles@gmx.net>
2024-08-02 17:42:50 +0200

    Migrate from webpack to rsbuild (#384)
    * Migrate from webpack to rsbuild
    
    https://rsbuild.dev/
    
    * Use relative instead of absolute path for assets
    
    * Only bind to localhost

---
+++ services/frontend/rsbuild.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, rspack } from '@rsbuild/core'
+import { pluginSass } from '@rsbuild/plugin-sass'
+import path from 'path'
+
+export default defineConfig(({ envMode }) => ({
+  source: {
+    entry: {
+      index: './src/ts/index.ts'
+    }
+  },
+  html: {
+    template: './src/templates/index.html'
+  },
+  output: {
+    assetPrefix: './'
+  },
+  plugins: [
+    pluginSass()
+  ],
+  tools: {
+    rspack: {
+      plugins: [
+        new rspack.DefinePlugin({
+          GROOVY_CONSOLE_SERVICE_URL: JSON.stringify(envMode === 'production' ? 'https://europe-west1-gwc-experiment.cloudfunctions.net/' : 'http://localhost:9080/'),
+          LOCAL_DEVELOPMENT: JSON.stringify(envMode === 'development')
+        }),
+        new rspack.CopyRspackPlugin({
+          patterns: [
+            { from: './src/static/', to: path.join(__dirname, 'dist') }
+          ]
+        })
+      ]
+    }
+  },
+  server: {
+    port: 9000,
+    host: 'localhost'
+  }
+}))
