45f623042230e6c901e9973d0219887ef812d730
Leonard Brünings <leonard.bruenings@gradle.com>
2025-12-18 18:04:04 +0100

    Update dependencies and refactor listRuntimes function for Google Cloud Functions Gen 2

--- functions/list_runtimes/index.js
+++ functions/list_runtimes/index.js
@@ -1,43 +1,74 @@
-const fetch = require('node-fetch')
+const { http } = require('@google-cloud/functions-framework');
+const { GoogleAuth } = require('google-auth-library');
+const fetch = require('node-fetch');
 
-exports.listRuntimes = async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
+http('listRuntimes', async (req, res) => {
+  // --- CORS Configuration ---
+  res.set('Access-Control-Allow-Origin', '*');
 
-  // based on https://cloud.google.com/functions/docs/samples/functions-http-cors?hl=de#functions_http_cors-nodejs
   if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET')
-    res.set('Access-Control-Allow-Headers', 'Content-Type')
-    res.set('Access-Control-Max-Age', '3600')
-    res.status(204).send('')
-  } else {
-    const { GoogleAuth } = require('google-auth-library')
-    const auth = new GoogleAuth()
-    const token = await auth.getAccessToken()
-    console.log('Token', token)
-
-    const project = 'gwc-experiment'
-    const location = 'europe-west1'
-    const parent = `projects/${project}/locations/${location}`
-
-    try {
-      const resp = await fetch(`https://cloudfunctions.googleapis.com/v1/${parent}/functions`, {
-        headers: {
-          'accept': 'application/json',
-          'content-type': 'application/json',
-          'authorization': `Bearer ${token}`
-        },
-      })
-      const json = await resp.json()
-      const functions = json.functions
-        .filter(fn => fn.name.indexOf('groovy') > -1)
-        .map(fn => fn.name)
-        .map(name => name.substring(name.indexOf('groovy_')))
-
-      res.status(200).send(functions)
-    } catch (e) {
-      console.error(e)
-      throw e
-    }
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.set('Access-Control-Max-Age', '3600');
+    return res.status(204).send('');
   }
-}
+
+  try {
+    // --- Authentication ---
+    // Use the Cloud Functions API scope explicitly
+    const auth = new GoogleAuth({
+      scopes: 'https://www.googleapis.com/auth/cloud-platform'
+    });
+    const client = await auth.getClient();
+    const tokenResponse = await client.getAccessToken();
+    const token = tokenResponse.token;
+
+    // --- Configuration ---
+    const project = 'gwc-experiment';
+    const location = 'europe-west1';
+
+    /**
+     * Note: Cloud Functions Gen 2 uses the v2 endpoint.
+     * Path structure: projects/{project}/locations/{location}/functions
+     */
+    const url = `https://cloudfunctions.googleapis.com/v2/projects/${project}/locations/${location}/functions`;
+
+    const resp = await fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    if (!resp.ok) {
+      const errorText = await resp.text();
+      console.error('API Error Response:', errorText);
+      return res.status(resp.status).send({ error: 'Failed to fetch functions from Google API' });
+    }
+
+    const json = await resp.json();
+
+    // Gen 2 API returns an object with a "functions" array
+    // We check if functions exist before processing
+    if (!json.functions) {
+      return res.status(200).send([]);
+    }
+
+    const functions = json.functions
+      .map(fn => fn.name) // name is in format projects/.../locations/.../functions/...
+      .filter(name => name.includes('groovy'))
+      .map(name => {
+        const parts = name.split('/');
+        const simpleName = parts[parts.length - 1];
+        return simpleName.includes('groovy_')
+          ? simpleName.substring(simpleName.indexOf('groovy_'))
+          : simpleName;
+      });
+
+    res.status(200).send(functions);
+  } catch (e) {
+    console.error('Function Execution Error:', e);
+    res.status(500).send({ error: 'Internal Server Error', message: e.message });
+  }
+});
